bf869d78025467abbca67ac2adfc09455b95c273
caiodiasol <caiodiasol@proton.me>
2025-10-08 20:19:26 -0300

    Fix: OrderViewSet test to correctly access list of products and categories

--- product/serializers/product_serialize.py
+++ product/serializers/product_serialize.py
@@ -1,12 +1,23 @@
 from rest_framework import serializers
 
+from product.models.category import Category
 from product.models.product import Product
 from product.serializers.category_serialize import CategorySerializer
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer(required=True, many=True)
+    category = CategorySerializer(read_only=True, many=True)
+    category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), write_only=True, many=True)
 
     class Meta:
         model = Product
-        fields = ['title', 'description', 'price', 'active', 'category']
+        fields = ['id', 'title', 'description', 'price', 'active', 'category', 'category_id']
+
+    def create(self, validated_data):
+        category_data = validated_data.pop('category_id')
+        
+        product = Product.objects.create(**validated_data)
+        for category in category_data:
+            product.category.add(category)
+        
+        return product
