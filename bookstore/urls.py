ac0ee852dd6bf20cfafa4edd014d96847738894b
caiodiasol <caiodiasol@proton.me>
2025-10-21 12:27:45 -0300

    Add home route for hello_world view

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path("api-token-auth/", obtain_auth_token, name="api_token_auth"),
     path("update_server/", views.update, name="update"),
     path("hello/", views.hello_world, name="hello"),
-    path('api/', include('api.urls')),
+    path("", views.hello_world, name="home"),  # Rota para a página inicial
 ]
 
 
